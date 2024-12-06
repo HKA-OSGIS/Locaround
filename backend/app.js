3d9a52ae1391f14016c36a3cfcb48de42418235d
LucilleGDBT <lucille.godbillot.auditeur@lecnam.net>
2024-12-06 21:04:11 +0100

    1.1

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const eventRoutes = require('./routes/events');
-const userRoutes = require('./routes/users');
-const recommendationRoutes = require('./routes/recommendations');
-const routeRoutes = require('./routes/routes');
-
+const { Pool } = require('pg');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
 
-// Routes
-app.use('/api/events', eventRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/recommendations', recommendationRoutes);
-app.use('/api/routes', routeRoutes);
+//Configuration of PostgreSQL database
+const pool = new Pool({
+    user: 'votre_utilisateur',
+    host: 'localhost',
+    database: 'votre_base_de_donnees',
+    password: 'votre_mot_de_passe',
+    port: 5432,
+});
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/eventmap', { useNewUrlParser: true, useUnifiedTopology: true });
+// Routes
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+// Exemple of route for events
+app.get('/api/events', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM events');
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
+});
+
+// Exemple de route pour le routage
+app.get('/api/routes', async (req, res) => {
+    const { start, end } = req.query;
+    const url = `http://localhost:5000/route/v1/driving/${start};${end}?overview=false&geometries=geojson`;
+
+    try {
+        const response = await axios.get(url);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).send('Error fetching route');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
